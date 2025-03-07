36dc1b374f519011dcbdb81513f6c55df874c53a
hritika1512 <hritikaail15@gmail.com>
2025-03-07 12:16:37 +0400

    Update main.py
    Fixed heights

--- main.py
+++ main.py
@@ -329,8 +329,8 @@ def mandela_component(color, brush_size, symmetry_lines):
         <script>
             const stage = new Konva.Stage({{
                 container: 'container',
-                width: 500,
-                height: 500,
+                width: 500;,
+                height: 500;,
             }});
             const layer = new Konva.Layer();
             stage.add(layer);
